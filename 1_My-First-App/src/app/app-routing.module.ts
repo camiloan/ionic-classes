2e32ebb5a0e4811b7a080c6b071894034299c941
camiloan <camiloa.andresc@hotmail.com>
2019-07-10 15:28:02 -0500

    Initial commit

---
+++ 1_My-First-App/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'contacto', loadChildren: './contacto/contacto.module#ContactoPageModule' },
+  { path: 'masinfo', loadChildren: './masinfo/masinfo.module#MasinfoPageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ 1_My-First-App/src/app/tab3/tab3.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+  lista: Array<any>=[
+    {
+      titulo:"Perro",
+      color:"#444"
+    },
+    {
+      titulo:"Gato",
+      color:"#999"
+    }
+  ]
+
+  constructor() {}
+
+}
