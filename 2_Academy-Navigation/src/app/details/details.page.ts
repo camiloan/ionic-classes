3834b5ef6d725ecb765471482fe9bc536f5d07bb
spartan117 <42893609+camiloan@users.noreply.github.com>
2019-07-12 01:33:19 -0500

    Academy-Navigation Success

--- 2_Academy-Navigation/src/app/details/details.page.ts
+++ 2_Academy-Navigation/src/app/details/details.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
+  data:any;
 
-  constructor() { }
-
+  constructor(private route: ActivatedRoute, private router:Router) { 
+    this.route.queryParams.subscribe(params =>{
+      console.log('params: ',params)
+      if(params && params.special){
+        this.data = JSON.parse(params.special);
+      }
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.data = this.router.getCurrentNavigation().extras.state.user;
+      }
+    }
+    );
+  }
   ngOnInit() {
+    if (this.route.snapshot.data['special']) {
+      this.data = this.route.snapshot.data['special'];
+    }
   }
 
 }
